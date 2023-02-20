fb65164cad092878b0151d6bbc370bedb776ab60
Vivacious <alajembaviviannkem@gmail.com>
2023-02-20 13:05:19 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  * Return: ALways 0
@@ -11,18 +12,18 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-		for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-			if (p == 98 && 9 == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && 9 == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
