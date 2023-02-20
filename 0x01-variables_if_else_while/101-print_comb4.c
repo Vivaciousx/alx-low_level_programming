6bda7693d63d58a8154511cc50ed2e18357216c7
Vivacious <alajembaviviannkem@gmail.com>
2023-02-20 12:44:19 +0100

    print comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -14,8 +14,10 @@ int main(void)
 
 	for (p = d + '0'; d < '9'; d++)
 	{
+
 	for (p = d + 1; p <= '9'; p++)
 	{
+
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != q) != q)
