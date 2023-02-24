fdb16fe4f603870e6a3cb734c6845713481c4b71
Vivacious <alajembaviviannkem@gmail.com>
2023-02-24 05:54:16 +0100

    Update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 
 /**
  * mul - Multiplies two integers
- * @x: The first number to multply 
+ * @x: The first number to multply
  * @y: The second number to multiply with first number
  * Return: The result of multiplication between x param and y param
  */
