18bf250f41cd7244d8bb6faffe74a015bd5b599e
Vivacious <alajembaviviannkem@gmail.com>
2023-02-24 04:29:42 +0100

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** _isdigit - Check if a character is a difit
+ * @x: The number to the checkers
+ * Return: 1 for a character that will be a digit or 0 forany else
+ */
+
+int _isdigit(int x)
+
+{
+	if (x >= 48 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
