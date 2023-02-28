99b5665cd3266ab1639bf0cbdef7e52db4cb537d
Vivacious <alajembaviviannkem@gmail.com>
2023-02-28 03:09:35 +0100

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenght of a string.
+ * @s: char to check the lenght of a string
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0; /* start the counter from 0*/
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
