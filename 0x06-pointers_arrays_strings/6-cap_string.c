b5a84c7c1a88057ad3a9bbf08671c88ef0e6eecc
Vivacious <alajembaviviannkem@gmail.com>
2023-03-02 05:22:38 +0100

    Update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 
 			index++;
 
-		if (str[index - 1] == '' ||
+		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
 		str[index - 1] == ',' ||
@@ -26,8 +26,8 @@ char *cap_string(char *str)
 		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
-		str[index - 1] == '}' ||
 		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
 		index == 0)
 			str[index] -= 32;
 		index++;
