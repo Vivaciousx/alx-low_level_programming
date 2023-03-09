fc19531ebcfe222c863cb972aeca0270baf186ac
Vivacious <alajembaviviannkem@gmail.com>
2023-03-09 16:53:40 +0100

    void _print_rev_recursion(char *s)

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that prints a string in reverse.
+ * @s: the string to be printed
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+}
+
