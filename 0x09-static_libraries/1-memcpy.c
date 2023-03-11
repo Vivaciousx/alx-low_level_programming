6b4694ab6a167fec768934c1edc9ca68af1de1c6
Vivacious <alajembaviviannkem@gmail.com>
2023-03-11 04:20:00 +0100

    My static library

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return memcpy(dest,src,n);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+0x09. C - Static libraries
+
+This README file contains tasks on Library and the two types of library, which are static and dynamic library.
